c20e76bfa914ba61f0c7a128d7e4a133a6950b43
Daniel Vela <de.vela.lopez@gmail.com>
2023-09-26 19:47:04 -0500

    Changed project to use Docker Compose

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: 'mysql',
     user: 'dbuser',
     password: 'insecurebank',
     database: 'bank'
