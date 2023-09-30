b692d994595774636286c63cb340f982111480e0
Daniel Vela <de.vela.lopez@gmail.com>
2023-09-30 05:44:31 -0500

    Changed infra, deployment process, Dockerfile, TF

--- main.tf
+++ main.tf
@@ -17,10 +17,152 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_ecr_repository" "insecurebank_ecr_web_repo" {
-    name = "insecurebank-web"  
+resource "aws_ecr_repository" "insecurebank_ecr_repo" {
+    name = "insecurebank"  
 }
 
-resource "aws_ecr_repository" "insecurebank_ecr_db_repo" {
-    name = "insecurebank-db"  
+resource "aws_ecs_cluster" "insecurebank_cluster" {
+  name = "insecurebank_cluster"
+}
+
+resource "aws_ecs_task_definition" "insecurebank_task" {
+  family = "insecurebank-task"
+  container_definitions = <<DEFINITION
+  [
+    {
+      "name": "insecurebank-task",
+      "image": "${aws_ecr_repository.insecurebank_ecr_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"]
+  network_mode = "awsvpc"
+  memory = 512
+  cpu = 256
+  execution_role_arn = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"] 
+
+    principals {
+      type = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_default_vpc" "default_vpc" {
+}
+
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_alb" "insecurebank_load_balancer" {
+  name               = "insecurebank-load-balancer"
+  load_balancer_type = "application"
+  subnets = [
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}"
+  ]
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}"
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.insecurebank_load_balancer.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+  }
+}
+
+resource "aws_ecs_service" "insecurebank_service" {
+  name = "insecurebank-service"
+  cluster = "${aws_ecs_cluster.insecurebank_cluster.id}"
+  task_definition = "${aws_ecs_task_definition.insecurebank_task.arn}"
+  launch_type = "FARGATE"
+  desired_count = 1
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+    container_name = "${aws_ecs_task_definition.insecurebank_task.family}"
+    container_port = 3000
+  }
+
+  network_configuration {
+    subnets = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+    assign_public_ip = true
+    security_groups = ["${aws_security_group.service_security_group.id}"]
+  }
+}
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "app_url" {
+  value = aws_alb.insecurebank_load_balancer.dns_name
 }
